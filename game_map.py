9b4850a4265cba1b567906375f929438f948dff6
Rodrigo Borges <rodrigo.sil.borges@gmail.com>
2022-05-24 17:15:06 -0300

    Implement part 4

--- game_map.py
+++ game_map.py
@@ -8,9 +8,13 @@ import tile_types
 class GameMap:
 
     def __init__(self, width:int, height:int):
+
         self.width = width
         self.height = height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
+
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        self.explored = np.full((width, height), fill_value=False, order="F")
 
     def in_bounds(self, x:int, y:int) -> bool:
         """
@@ -19,4 +23,16 @@ class GameMap:
         return 0 <= x < self.width and 0 <= y < self.height
     
     def render(self, console:Console) -> None:
-        console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+        Render the map
+
+        If a tile is in the "visible" array, then draw it with the "light colors
+        If it isn't, but it's in the "explored" array, then draw it with the "dark" colors
+        Otherwise, the default is "SHROUD"
+
+        """
+        console.rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
